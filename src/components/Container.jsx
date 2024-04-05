b446b9e2164a01c43d3dd44a4d689d44aba45867
szvitek <gy.szvitek@gmail.com>
2024-04-05 17:42:19 +0100

    feat(Constants): Extract Constants into a Separate File fix css

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import Stats from './Stats';
 import Textarea from './Textarea';
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from '../lib/constants';
 
 export default function Container() {
   const [text, setText] = useState('');
   const stats = {
     numberOfWords: text.split(/\s/).filter((word) => word).length,
     numberOfCharacters: text.length,
-    igCharsLeft: 280 - text.length,
-    fbCharsLeft: 2200 - text.length,
+    igCharsLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    fbCharsLeft: FACEBOOK_MAX_CHARACTERS - text.length,
   };
 
   return (
