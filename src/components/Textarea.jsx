46d27f08bc0a3d159f9dfb767e79351ca149421d
szvitek <gy.szvitek@gmail.com>
2024-04-05 15:15:43 +0100

    feat(Textarea): controlled input, basic validation, reusable warning component

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,3 +1,35 @@
+import { useState } from 'react';
+import Warning from './Warning';
+
 export default function Textarea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState('');
+  const [warningText, setWarningText] = useState('');
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+
+    // basic validation
+    if (newText.includes('<script>')) {
+      setWarningText('No script tag allowed!');
+      newText = newText.replace('<script>', '');
+    } else if (newText.includes('@')) {
+      setWarningText('No @ symbol allowed!');
+      newText = newText.replace('@', '');
+    } else {
+      setWarningText('');
+    }
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
+      {warningText && <Warning warningText={warningText} />}
+    </div>
+  );
 }
