c6d3829b3a7d1d2b1c508fc1b7878b29730b337e
szvitek <gy.szvitek@gmail.com>
2024-04-05 14:35:25 +0100

    feat(App): Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Container from "./components/Container"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 function App() {
   return (
     <>
-      test
+      <Header />
+      <Container />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import BackgroundImage from './BackgroundImage';
+import Title from './Title';
+
+export default function Header() {
+  return (
+    <header>
+      <BackgroundImage />
+      <Title />
+    </header>
+  );
+}
